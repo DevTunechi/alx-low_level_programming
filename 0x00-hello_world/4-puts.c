6f22a2aa33bfb27263079444ca13da7b7cd1e25d
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-05 08:08:16 -0700

    4 Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- This is the main function
+ * Return: Always return zero
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
