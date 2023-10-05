a6989a26143b30b7df9d93a8f136f6d1ba449c61
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-05 10:18:52 -0700

    6 Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
 /**
- * Main- Sizes of Char, int, long int, long long int
+ * main- Sizes of Char, int, long int, long long int
  * Return: Always return zero
  */
 
 int main(void)
+
 {
 char c;
 int i;
 long int li;
 long long int lli;
+float f;
+
 printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
 printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
 printf("The size of an long int is: %lu.\n", (unsigned long)sizeof(li));
 printf("The size of an long long int is: %lu.\n", (unsigned long)sizeof(lli));
+frintf("The size of an float is: %lu.\n", (unsigned long)sizeof(f));
 return (0);
 }
