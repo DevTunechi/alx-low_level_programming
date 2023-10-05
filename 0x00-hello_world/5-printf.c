0d4c55249fbed46574608066960f70807da7a091
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-05 08:26:10 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- Project printf
+ * Return: Must always return zer0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
