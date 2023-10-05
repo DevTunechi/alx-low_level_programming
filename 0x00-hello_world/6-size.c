27cfc3b17ed9bfd5708e56ded6ee09ffae6e2b08
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-05 10:42:10 -0700

    6 Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,10 @@ int i;
 long int li;
 long long int lli;
 float f;
-
 printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
 printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
 printf("The size of an long int is: %lu.\n", (unsigned long)sizeof(li));
 printf("The size of an long long int is: %lu.\n", (unsigned long)sizeof(lli));
-frintf("The size of an float is: %lu.\n", (unsigned long)sizeof(f));
+printf("The size of an float is: %lu.\n", (unsigned long)sizeof(f));
 return (0);
 }
