64d39dabb6ac58a069da9a5c3f831f88301e2961
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-06 15:27:19 -0700

    Print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,12 @@
 int main(void)
 {
 char a;
-for (a = 'a'; a <= 'z'; a++)
+for (a = 'a'; a <= 'z' || a <= 'Z'; a++)
+{
 putchar(a);
-for (a = 'A'; a <= 'Z'; a++)
-putchar(a);
+if (a == 'Z')
+a = 'A' - 1;
+}
 putchar('\n');
 return (0);
 }
