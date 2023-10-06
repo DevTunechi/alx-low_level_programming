649610963a129cd4a13e28c5980cca2932026831
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-06 10:09:44 -0700

    P or N

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,17 +3,18 @@
 #include <time.h>
 
 /**
- * main- printing positive or negative numbers
+ * main- Printing positive and negative number
  * Return: Always return zero
  */
+
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n>0)
+if (n > 0)
 printf("%d is positive\n", n);
-else if(n<0)
+else if (n < 0)
 printf("%d is negative\n", n);
 else
 printf("%d is zero\n", n);
