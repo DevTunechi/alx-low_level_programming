a8701afb4d0435108533d7cdb094a86270c26c83
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-06 14:35:07 -0700

    1 Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ n = rand() - RAND_MAX / 2;
 endn = n % 10;
 if (endn > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n ", n, endn);
+printf("Last digit of %d is %d and is greater than 5\n", n, endn);
 }
 else if (endn == 0)
 {
