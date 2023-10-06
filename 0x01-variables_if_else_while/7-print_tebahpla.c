8787416ef6f519143f410818ce3ae994625a070d
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-06 16:43:17 -0700

    Print Tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - input point
+ * Return: Always returns 0
+ */
+
+int main(void)
+
+{
+char a;
+for (a = 'z'; a >= 'a'; a--)
+putchar(a);
+putchar('\n');
+return (0);
+}
