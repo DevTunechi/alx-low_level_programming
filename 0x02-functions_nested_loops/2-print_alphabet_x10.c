10833e91654e209641f76105432be93bae5424fc
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-10 08:08:27 -0700

    10x Alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,15 @@
  * Return: Always 0.
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 int i;
 char letter = 'a';
+for (i = 0; i < 10; i++)
+{
+}
 for (letter = 'a'; letter <= 'z'; letter++)
 {_putchar(letter);
 }
-for (i = 0; i < 10; i++)
 _putchar('\n');
 }
