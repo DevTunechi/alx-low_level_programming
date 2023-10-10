03b272eb909dba2c4c9023b6e3177ea8f48a5420
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-10 07:37:04 -0700

    10x Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - this is the function that prints alphabets x10
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+char letter = 'a';
+for (i = 0; i < 10; i++)
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+}
+
