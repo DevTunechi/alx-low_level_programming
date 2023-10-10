027d4868a8f9efaf82bfde45d93b10005b914af9
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-10 05:31:14 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
-#include "main.h"
+#ifndef "main.h"
+
 /**
- *  main- The Entry point
- *  Return: Always return (0)
+ * main - Entry point 
+ * Return: Always 0 
  */
 
-
-int main(void)
+int mai(void)
 
 {
-_putchar("_putchar\n");
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n")
+	
 return (0);
-
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Check the code
+ *
+ * letter: Starts from lowercase a-z
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+
+/**
+ * print_alphabet- prints letters in lovwercase
+ */
+
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+
+}
