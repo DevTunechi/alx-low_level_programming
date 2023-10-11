76bcfd959a0f225179ac9819a2a06aa25da3ad2c
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-11 13:28:26 -0700

    101 natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 /**
-  * main - This function prints the first 52 fib
-  * Return: Always return 0.
-  */
-
+ * main - This function finds and prints the Fibonacci number 244293.
+ * Return: Always return 0.
+ */
 
 int main(void)
 
 {
 int i = 0;
 long j = 1, k = 2;
+bool found = false;
 
-while (i < 50)
+while (i < 52)
 {
 if (i == 0)
 printf("%ld", j);
@@ -24,8 +25,25 @@ k += j;
 j = k - j;
 printf(", %ld", k);
 }
+
+if (k == 244293)
+{
+found = true;
+break;
+}
+
 ++i;
 }
-printf("\n");
+
+if (found)
+{
+printf("\n(7 chars long)\n");
+}
+else
+{
+fprintf(stderr, "[Anything]\n");
+}
+
 return (0);
 }
+
