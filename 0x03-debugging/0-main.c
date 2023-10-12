35e3c7272bea2dafab9d8faa52a3e6d0f02a88a7
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-12 10:06:14 -0700

    main c, h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,22 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
- * main- Printing positive and negative number
- * Return: Always return zero
+ * main - tests function that prints if integer is positive or negative
+ *
+ * Return:0
  */
 
 int main(void)
+
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
-else
-printf("%d is zero\n", n);
+int i;
+
+i = 98;
+positive_or_negative(i);
 return (0);
 }
