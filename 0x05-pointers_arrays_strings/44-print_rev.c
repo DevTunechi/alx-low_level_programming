47dc2fdabe3287ac18984f8aba26060c96296338
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-16 13:16:19 -0700

    4 print rev

--- 0x05-pointers_arrays_strings/44-print_rev.c
+++ 0x05-pointers_arrays_strings/44-print_rev.c
@@ -3,8 +3,18 @@
 /**
  * print_rev - Prints a string in reverse followed by a new line
  *
- * @s: Pointer to the string to be printed in reverse
+ * _putchar - Writes a character to stdout
+ *
+ * @s - Character to be printed
+ *
+ * @s: Pointer to the string to be printed
+ *
+ * Return: On success 1.
+ *
+ * On error, -1 is returned, and errn is set appropriately.
  */
+
+int _putchar(char s);
 
 void print_rev(char *s)
 
@@ -23,7 +33,5 @@ while (length > 0)
 length--;
 _putchar(s[length]);
 }
-
 _putchar('\n');
 }
-
