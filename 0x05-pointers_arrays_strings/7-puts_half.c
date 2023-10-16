7a17d86accc14c28a4d9e1344f806e18e9d1bdf4
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-16 15:04:07 -0700

    7 puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string followed by a new line
+ *
+ * @str: Pointer to the string to be printed
+ */
+
+void puts_half(char *str)
+
+{
+int length = 0;
+int i, start;
+
+while (str[length] != '\0')
+{
+length++;
+}
+if (length % 2 == 0)
+{
+start = length / 2;
+}
+else
+{
+start = (length - 1) / 2;
+}
+for (i = start; i < length; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
