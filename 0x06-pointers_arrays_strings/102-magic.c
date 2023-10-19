41039a2b7cd87f99d6786ecedfb14ebbcb8517c3
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-19 08:04:12 -0700

    102-magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -17,6 +17,7 @@ a[2] = 1024;
 p = &n;
 
 *(p + 5) = 98;
+
 /*
 * write your line of code here...
 *(p +5) = 98;
@@ -27,6 +28,7 @@ p = &n;
 * - you are not allowed to code anything else than this line of code
 */
 ;
+
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
