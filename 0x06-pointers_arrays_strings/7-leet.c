03715e92e1f69c708941cccf85ae67c74ee8a9a8
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-19 07:50:18 -0700

    7 leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,42 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * leet - Encodes a string into 1337.
- * @str: The input string.
- *
- * Description: Encodes the input string based on the following replacements:
- * 'a' and 'A' with '4', 'e' and 'E' with '3', 'o' and 'O' with '0',
- * 't' and 'T' with '7', 'l' and 'L' with '1'.
- * Return: A pointer to the modified string.
+  *leet - This is the function name
+  * @n: The function paramete
+  *Return: Value of n
  */
 
-char *leet(char *str)
+char *leet(char *n)
 
 {
-char leet_map[256] = {0};
+int f, j;
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
 
-for (int i = 0; i < 256; i++)
+for (f = 0; n[f] != '\0'; f++)
 {
-leet_map[i] = (char)i;
+for (j = 0; j < 10; j++)
+{
+if (n[f] == s1[j])
+{
+n[f] = s2[j];
 }
-
-leet_map['a'] = '4';
-leet_map['A'] = '4';
-leet_map['e'] = '3';
-leet_map['E'] = '3';
-leet_map['o'] = '0';
-leet_map['O'] = '0';
-leet_map['t'] = '7';
-leet_map['T'] = '7';
-leet_map['l'] = '1';
-leet_map['L'] = '1';
-
-char *ptr = str;
-while (*ptr != '\0')
-{
-*ptr = leet_map[(unsigned char)(*ptr)];
-ptr++;
 }
-
-return (str);
+}
+return (n);
 }
