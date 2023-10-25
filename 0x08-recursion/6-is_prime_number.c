28c2ede50de1849e50b0992fa1e8fc7284e71efb
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-25 11:07:05 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,7 +5,7 @@ int check_prime(int n, int divisor);
 /**
  * is_prime_number - Checks if a number is prime.
  * @n: The input number.
- * 
+ *
  * Return: 1 if n is a prime number, 0 otherwise.
  */
 int is_prime_number(int n)
@@ -21,7 +21,7 @@ return (check_prime(n, 2));
  * check_prime - Helper function to check if a number is prime using recursion.
  * @n: The input number.
  * @divisor: The current divisor to check for divisibility.
- * 
+ *
  * Return: 1 if n is a prime number, 0 otherwise.
  */
 int check_prime(int n, int divisor)
