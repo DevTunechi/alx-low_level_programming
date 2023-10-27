292951a86f35a6549e212ee11a389bdd221a4269
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-10-27 16:27:47 -0700

    3 mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed
+ * @argv: Array of pointers to strings
+ *
+ * Return: 0 on Success or 1 for Error
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 * num2;
+
+printf("%d\n", result);
+return (0);
+}
