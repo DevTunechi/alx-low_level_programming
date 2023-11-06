4cd5a952cd3baaf11ca4d00ee93e93dad7eda813
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-11-06 06:57:29 -0800

    Unconditional Dog Love

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * print_dog - Prints the elements of struct dog
+  * @d: Pointer to the struct dog variable
+  */
+
+void print_dog(struct dog *d)
+{
+if (d != NULL)
+{
+printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+printf("Age: %.6f\n", d->age);
+printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+}
+}
