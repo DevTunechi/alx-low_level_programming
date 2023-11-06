5cdbc3816fa9925986f241611482aee7d52afeed
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-11-06 08:03:42 -0800

    Dogs love you

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,26 +1,20 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * init_dog - Initializes a struct dog variable
- * @d: Pointer to the struct dog variable
- * @name: Pointer to string representing the dog's name
- * @age: The dog's age
- * @owner: Pointer to string representing dog owner's name
+ * init_dog - Variable type struct dog
+ * @d: The pointer
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
+
 {
-if (d != NULL)
-{
-d->name = name;
+if (d == NULL)
+return;
+(*d).name = name;
 d->age = age;
 d->owner = owner;
-}
-else
-{
-printf("Error: Invalid pointer to struct dog\n");
-exit(EXIT_FAILURE);
-}
 }
