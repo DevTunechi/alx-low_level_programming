81d0436f733a2b22f39d94f25368483d908dab7b
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-11-27 11:25:38 -0800

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,69 +2,124 @@
 
 /**
  * main - Entry point of the program.
- * @argc: Number of arguments.
- * @argv: Array of argument strings.
  * Return: Exit status.
  */
 
+/**
+ * display_usage - Displays the correct usage of the program
+ * @program_name: The name of the program
+ */
+
+void display_usage(char *program_name);
+int open_source_file(char *filename);
+int open_destination_file(char *filename);
+void copy_file_content(int source_fd, int dest_fd);
+void handle_error(char *message, char *filename, int source_fd, int dest_fd);
+
 int main(int argc, char *argv[])
 {
-int fd_from, fd_to;
-ssize_t bytes_read, bytes_written;
-char buffer[BUFFER_SIZE];
+int source_fd, dest_fd;
 
 if (argc != 3)
 {
-dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+display_usage(argv[0]);
 exit(97);
 }
 
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
-{
-dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+source_fd = open_source_file(argv[1]);
+dest_fd = open_destination_file(argv[2]);
+
+copy_file_content(source_fd, dest_fd);
+
+close(source_fd);
+close(dest_fd);
+
+return (0);
 }
 
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (fd_to == -1)
+/**
+ * display_usage - Displays the correct usage of the program.
+ * @program_name: The name of the program.
+ */
+
+void display_usage(char *program_name)
 {
-dprintf(2, "Error: Can't write to %s\n", argv[2]);
-close(fd_from);
-exit(99);
+dprintf(2, "Usage: %s file_from file_to\n", program_name);
 }
 
-while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+/**
+ * open_source_file - Opens the source file for reading.
+ * @filename: The name of the source file.
+ * Return: The file descriptor of the opened source file.
+ */
+
+int open_source_file(char *filename)
 {
-bytes_written = write(fd_to, buffer, bytes_read);
+int source_fd = open(filename, O_RDONLY);
+if (source_fd == -1)
+{
+handle_error("Error: Can't read from file", filename, -1, -1);
+}
+return (source_fd);
+}
+
+/**
+ * open_destination_file - Opens the destination file for writing.
+ * @filename: The name of the destination file.
+ * Return: The file descriptor of the opened destination file.
+ */
+
+int open_destination_file(char *filename)
+{
+int dest_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (dest_fd == -1)
+{
+handle_error("Error: Can't write to", filename, -1, -1);
+}
+return (dest_fd);
+}
+
+/**
+ * copy_file_content - Copies the content from source file to destination file.
+ * @source_fd: File descriptor of the source file.
+ * @dest_fd: File descriptor of the destination file.
+ */
+
+void copy_file_content(int source_fd, int dest_fd)
+{
+ssize_t bytes_read, bytes_written;
+char buffer[BUFFER_SIZE];
+
+while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
+{
+bytes_written = write(dest_fd, buffer, bytes_read);
 if (bytes_written == -1 || bytes_written != bytes_read)
 {
-dprintf(2, "Error: Can't write to %s\n", argv[2]);
-close(fd_from);
-close(fd_to);
-exit(99);
+handle_error("Error: Can't write to", "", source_fd, dest_fd);
 }
 }
 
 if (bytes_read == -1)
 {
-dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-close(fd_from);
-close(fd_to);
-exit(98);
+handle_error("Error: Can't read from file", "", source_fd, dest_fd);
+}
 }
 
-if (close(fd_from) == -1)
+/**
+ * handle_error - Handles errors, displays an error message, and exits the program.
+ * @message: The error message.
+ * @filename: The relevant filename.
+ * @source_fd: File descriptor of the source file.
+ * @dest_fd: File descriptor of the destination file.
+ */
+
+void handle_error(char *message, char *filename, int source_fd, int dest_fd)
+
 {
-dprintf(2, "Error: Can't close fd %d\n", fd_from);
-exit(100);
-}
-
-if (close(fd_to) == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", fd_to);
-exit(100);
-}
-
-return (0);
+dprintf(2, "%s %s\n", message, filename);
+if (source_fd != -1)
+close(source_fd);
+if (dest_fd != -1)
+close(dest_fd);
+exit(99);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,10 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 size_t _strlen(char *str);
+void display_usage(char *program_name);
+int open_source_file(char *filename);
+int open_destination_file(char *filename);
+void copy_file_content(int source_fd, int dest_fd);
+void handle_error(char *message, char *filename, int source_fd, int dest_fd);
 
 #endif
