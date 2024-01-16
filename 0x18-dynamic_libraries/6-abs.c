4e632a0ae9fcaf1c146ab268a6239bb13e8e9bec
DevTunechi <tunjidare2@yahoo.com>
2024-01-15 18:46:02 -0800

    5-strstr.c 6-abs.c 9-strcpy.c

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of an integer
+ * @n: n is an integer
+ * Olatunji Oluwadare
+ * Return: integer
+ */
+
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
